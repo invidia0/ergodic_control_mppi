84049b98a48f9b8596653d78c1c2cf807ec7df59
MatCat960 <mattia.catellani@unimore.it>
2025-12-29 14:14:15 +0100

    fix torch dependencies

--- mppi/torch/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-if sys.version_info >= (3, 9):
-    raise RuntimeError(
-        "Torch backend currently supports only Python 3.8. "
-        "Please use Python 3.8 or install the JAX backend."
-    )
